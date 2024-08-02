4dac2e572faa73a727efeb5a2db08eaa694c1ed9
Wizard <cristiandamianvena@gmail.com>
2024-08-01 22:22:30 -0300

    fix: removed dead code

--- src/main/kotlin/example/com/http/router/UserRouter.kt
+++ src/main/kotlin/example/com/http/router/UserRouter.kt
@@ -74,36 +74,5 @@ fun Application.userRouter() {
 
             call.respond(HttpStatusCode.NoContent)
         }
-
-
-        /*
-        // Create car
-        post("/cars") {
-            val car = call.receive<Car>()
-            val id = carService.create(car)
-            call.respond(HttpStatusCode.Created, id)
-        }
-        // Read car
-        get("/cars/{id}") {
-            val id = call.parameters["id"] ?: throw IllegalArgumentException("No ID found")
-            carService.read(id)?.let { car ->
-                call.respond(car)
-            } ?: call.respond(HttpStatusCode.NotFound)
-        }
-        // Update car
-        put("/cars/{id}") {
-            val id = call.parameters["id"] ?: throw IllegalArgumentException("No ID found")
-            val car = call.receive<Car>()
-            carService.update(id, car)?.let {
-                call.respond(HttpStatusCode.OK)
-            } ?: call.respond(HttpStatusCode.NotFound)
-        }
-        // Delete car
-        delete("/cars/{id}") {
-            val id = call.parameters["id"] ?: throw IllegalArgumentException("No ID found")
-            carService.delete(id)?.let {
-                call.respond(HttpStatusCode.OK)
-            } ?: call.respond(HttpStatusCode.NotFound)
-        } */
     }
 }
